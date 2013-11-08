2cfd0e1d31e5357288448ab273a383d7c1237c1a
Pete Field <pedalpete@hotmail.com>
2013-11-08 11:21:52 +1100

    added socket.io-client to create a connection to the server on start

--- module/connect.js
+++ module/connect.js
@@ -1,23 +1,14 @@
 var fs = require("fs");
 var config = JSON.parse(fs.readFileSync("./config.json"));
-var ext_request = require("request");
-
+var socket = require('socket.io-client');
+var io = socket.connect('http://dev.konscio:3000', {reconnect: true});
 function Connect(){
-	this.config = config;
-	this.send_connection = function(){
-		var url='http://dev.konscio:3000/connections';
-		ext_request.post(url,{form:this.config}, function(err,resp,body){
-		 res.type('json');
-		if(err){
-			console.log('error');
-			return res.jsonp({error: "dang! There was a probem getting the wikipedia page. Is it possible wikipedia doesn't know what "+req.params.query+" is?"});
-		} else {
-			return true;
-		}
-	});
-		
-	}
+		var that=this;
+		io.emit('config',{config:config});
+		io.on('ready', function(data){
+			that.returned_config=data;
+		});
 	
-}
+};
 
 module.exports=Connect;
--- spec/init-spec.js
+++ spec/init-spec.js
@@ -3,14 +3,12 @@ var $$=require("../module/module.js");
 describe("initializing connection",function(){
 	it("should get the config file", function(){
 		var connection =  new $$.Connect();
-		expect(connection.config.name).toEqual("tri-color_led");
+		waits(3000);
+
+		runs(function(){
+		console.log(connection);
+		expect(connection.returned_config.name).toEqual("tri-color_led");
+		});
 	});
 });
 
-describe("request connection", function(){
-	it("should send a request to connect", function(){
-		var connection = new $$.Connect().send_connection();
-
-	
-	});
-});
